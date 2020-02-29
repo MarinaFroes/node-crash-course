ba7a36ead09b20f9aefaec3e61fc5583b5a859cb
MFAC <ninafroes@gmail.com>
2020-02-29 13:47:28 +0100

    feat: Log host and hostname to the console

--- reference/url_demo.js
+++ reference/url_demo.js
@@ -1,8 +1,13 @@
 const url = require('url');
 
-const myUrl = new URL('https://mywebsite.com/hello.html?id=100&status=active');
+const myUrl = new URL('https://mywebsite.com:8000/hello.html?id=100&status=active');
 
 // Serialized URL
 console.log(myUrl.href);
 console.log(myUrl.toString());
 
+// Host (root domain)
+console.log(myUrl.host);
+
+// Hostname (root domain name) - hostname doesn't include the port
+console.log(myUrl.hostname);
