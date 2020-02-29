7d5c40997a54fe364c7100ecd9baedbf3dc69e0d
MFAC <ninafroes@gmail.com>
2020-02-29 11:21:40 +0100

    feat: Invoke path.dirname to log to the console only the directory name from the __filename

--- reference/path_demo.js
+++ reference/path_demo.js
@@ -2,3 +2,6 @@ const path = require('path');
 
 // Base file name
 console.log(path.basename(__filename));
+
+// Directory name - same result as using __dirname
+console.log(path.dirname(__filename));
