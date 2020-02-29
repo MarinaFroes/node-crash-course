f2a6b980448d202bc6f8ad1a614edc0508a28796
MFAC <ninafroes@gmail.com>
2020-02-29 11:50:18 +0100

    feat: Add fs.append to append file to hello.txt

--- reference/fs_demo.js
+++ reference/fs_demo.js
@@ -3,16 +3,26 @@ const path = require('path');
 
 // Create a folder on the system using file system core module
   // The empty object as second argument is for options, but we don't have any
-fs.mkdir(path.join(__dirname, '/test'), {}, err => {
-  if (err) throw err;
-  console.log('Folder created');
-}); //assync but default
+// fs.mkdir(path.join(__dirname, '/test'), {}, err => {
+//   if (err) throw err;
+//   console.log('Folder created');
+// }); 
 
-// Create and write to file
+// Create and write to file - overwrites everything on the file
 fs.writeFile(path.join(__dirname, '/test', 'hello.txt'),
   'Hello World!',
   err => {
     if (err) throw err;
     console.log('File written to...');
+  
+    // File append
+    fs.appendFile(path.join(__dirname, '/test', 'hello.txt'),
+      ' I love Node.js',
+      err => {
+        if (err) throw err;
+        console.log('File written to...');
+      }
+    )
   }
 ); //assync but default
+
