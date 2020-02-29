abe1e4bb088d9b32ada0e7c75dd671e24896bb38
MFAC <ninafroes@gmail.com>
2020-02-29 15:19:15 +0100

    feat: Create a server and listen it to a PORT - either an environmental variable or the port 5000

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
-const Logger = require('./logger');
+const http = require('http');
+const path = require('path');
+const fs = require('fs');
 
-// Logger is a class, so we need to instantiate it
-const logger = new Logger();
-// Adding event listener
-logger.on('message', (data) => console.log('Called listener:', data));
-// Calling the log method of the logger instance
-logger.log('Hello World');
+const server = http.createServer((req, res) => {
+
+})
+
+// When we deploy it, tt's not going to run in the same port everytime
+  // The host will decide the port - assign it to an environmental variable
+  // If the environmental variable is not found, it's going to run in the 5000 port
+const PORT = process.env.PORT || 5000;
+
+server.listen(PORT, () => console.log(`Server running on PORT: ${PORT}`));
