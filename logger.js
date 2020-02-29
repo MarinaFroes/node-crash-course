a19f69d7a9c994ed6d40cd49cf09c214f45c0e23
MFAC <ninafroes@gmail.com>
2020-02-29 15:04:41 +0100

    refactor: Remove code from index.js and paste it here as reference

--- logger.js
+++ logger.js
@@ -8,4 +8,15 @@ class Logger extends EventEmitter{
   }
 }
 
-module.exports = Logger;
+// Export it to another file
+// module.exports = Logger;
+
+// In the new file, require logger before instantiate it
+// const Logger = require('./logger');
+
+// Logger is a class, so we need to instantiate it
+const logger = new Logger();
+// Adding event listener
+logger.on('message', (data) => console.log('Called listener:', data));
+// Calling the log method of the logger instance
+logger.log('Hello World');
